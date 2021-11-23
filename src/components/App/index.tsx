e6adadb884d96a22ce2564d3f1f867c4ba166ef1
Maciej Zarzeczny <maciej.zarzeczny@htdevelopers.com>
2021-11-23 11:40:18 +0100

    Setup absolute paths

---
+++ src/components/App/index.tsx
@@ -0,0 +1,9 @@
+const App = () => {
+    return (
+        <div className="App">
+            <p>it is working</p>
+        </div>
+    );
+};
+
+export default App;
--- src/index.tsx
+++ src/index.tsx
@@ -1,13 +1,13 @@
 import { StrictMode } from 'react';
 import ReactDOM from 'react-dom';
-import App from './App';
-import reportWebVitals from './reportWebVitals';
-import {GlobalStyle} from './theme/globalStyles';
+import App from 'components/App';
+import reportWebVitals from 'reportWebVitals';
+import { GlobalStyle } from 'theme/globalStyles';
 
 ReactDOM.render(
     <StrictMode>
         <GlobalStyle />
-            <App />
+        <App />
     </StrictMode>,
     document.getElementById('root'),
 );
