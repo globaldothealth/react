b5a555149530fe9a0e4a58624006ca9b94879d30
Oskar Kocjan <oskarkocjan@Oskar-Kocjan.local>
2022-07-25 12:04:27 +0200

    added filter that removes countries with no data from map layer

--- src/containers/CountryView/index.tsx
+++ src/containers/CountryView/index.tsx
@@ -211,6 +211,13 @@ const CountryView: React.FC = () => {
             'waterway-label',
         );
 
+        //Filter out countries without any data
+        mapRef.setFilter('countries-join', [
+            'in',
+            'iso_3166_1',
+            ...countriesData.map((country) => country['code']),
+        ]);
+
         // Change the mouse cursor to pointer when hovering above this layer
         mapRef.on('mouseenter', 'countries-join', () => {
             mapRef.getCanvas().style.cursor = 'pointer';
