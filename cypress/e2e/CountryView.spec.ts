cedb617445e128a639c141da50cae9793f32225a
Maciej Zarzeczny <maciej.zarzeczny@htdevelopers.com>
2022-10-06 10:54:11 +0200

    Updated tests

--- cypress/e2e/CountryView.spec.ts
+++ cypress/e2e/CountryView.spec.ts
@@ -1,6 +1,16 @@
 describe('<CountryView />', () => {
+    beforeEach(() => {
+        cy.intercept(
+            'GET',
+            'https://covid-19-aggregates-dev.s3.eu-central-1.amazonaws.com/country/latest.json',
+            { fixture: 'countriesData.json', statusCode: 200 },
+        ).as('fetchCountriesData');
+    });
+
     it('Displays map and legend', () => {
         cy.visit('/');
+
+        cy.wait('@fetchCountriesData');
 
         cy.get('.mapboxgl-canvas').should('be.visible');
         cy.contains('Line List Cases').should('be.visible');
--- cypress/e2e/CoverageView.spec.ts
+++ cypress/e2e/CoverageView.spec.ts
@@ -1,6 +1,16 @@
 describe('<CoverageView />', () => {
+    beforeEach(() => {
+        cy.intercept(
+            'GET',
+            'https://covid-19-aggregates-dev.s3.eu-central-1.amazonaws.com/country/latest.json',
+            { fixture: 'countriesData.json', statusCode: 200 },
+        ).as('fetchCountriesData');
+    });
+
     it('Displays map and legend', () => {
         cy.visit('/coverage');
+
+        cy.wait('@fetchCountriesData');
 
         cy.get('.mapboxgl-canvas').should('be.visible');
         cy.contains('Coverage').should('be.visible');
